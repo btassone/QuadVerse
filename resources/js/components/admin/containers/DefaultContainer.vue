d64631395cf7444a4b423eb10b357849afd56649
Brandon Tassone <brandontassone@gmail.com>
2018-10-19 03:27:44 -0400

    Added sidebar list items

--- resources/js/components/admin/containers/DefaultContainer.vue
+++ resources/js/components/admin/containers/DefaultContainer.vue
@@ -7,11 +7,11 @@
         <img class="navbar-brand-minimized" src="../assets/img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
       </b-link>
       <SidebarToggler class="d-md-down-none" display="lg" />
-      <b-navbar-nav class="d-md-down-none">
-        <b-nav-item class="px-3" to="/admin/dashboard">Dashboard</b-nav-item>
-        <b-nav-item class="px-3" to="/admin/users" exact>Users</b-nav-item>
-        <b-nav-item class="px-3" to="/admin/settings">Settings</b-nav-item>
-      </b-navbar-nav>
+      <!--<b-navbar-nav class="d-md-down-none">-->
+        <!--<b-nav-item class="px-3" to="/admin/dashboard">Dashboard</b-nav-item>-->
+        <!--<b-nav-item class="px-3" to="/admin/users" exact>Users</b-nav-item>-->
+        <!--<b-nav-item class="px-3" to="/admin/settings">Settings</b-nav-item>-->
+      <!--</b-navbar-nav>-->
       <b-navbar-nav class="ml-auto">
         <b-nav-item class="d-md-down-none">
           <i class="icon-bell"></i>
@@ -62,7 +62,7 @@
 </template>
 
 <script>
-// import nav from '@/_nav'
+import nav from "../navigation/List";
 import { Header as AppHeader, SidebarToggler, Sidebar as AppSidebar, SidebarFooter, SidebarForm, SidebarHeader, SidebarMinimizer, SidebarNav, Aside as AppAside, AsideToggler, Footer as TheFooter, Breadcrumb } from '@coreui/vue'
 import DefaultAside from './DefaultAside'
 import DefaultHeaderDropdownAccnt from './DefaultHeaderDropdownAccnt'
@@ -87,7 +87,7 @@ export default {
   },
   data () {
     return {
-    	nav: []
+	    nav: nav.items
     }
   },
   computed: {
---
+++ resources/js/components/admin/navigation/List.js
@@ -0,0 +1,86 @@
+export default {
+	items: [
+		{
+			title: true,
+			name: 'User',
+			class: '',
+			wrapper: {
+				element: '',
+				attributes: {}
+			}
+		},
+		{
+			name: 'Profile',
+			url: '/admin/user/profile',
+			icon: 'icon-user',
+			children: [
+				{
+					name: 'Clubs',
+					url: '/admin/user/profile/clubs',
+					icon: 'icon-people'
+				},
+				{
+					name: 'Certs',
+					url: '/admin/user/profile/certs',
+					icon: 'icon-check'
+				},
+				{
+					name: 'Information',
+					url: '/admin/user/profile/information',
+					icon: 'icon-info'
+				}
+			]
+		},
+		{
+			name: 'Blog',
+			url: '/admin/user/blog',
+			icon: 'icon-note',
+			children: [
+				{
+					name: 'Posts',
+					url: '/admin/user/blog/posts',
+					icon: 'icon-list'
+				},
+				{
+					name: 'Media',
+					url: '/admin/user/blog/media',
+					icon: 'icon-camrecorder'
+				}
+			]
+		},
+		{
+			name: 'Quads',
+			url: '/admin/user/quads',
+			icon: 'icon-rocket',
+			children: [
+				{
+					name: 'List',
+					url: '/admin/user/quads/list',
+					icon: 'icon-list'
+				},
+			]
+		},
+		{
+			name: 'Flying',
+			url: '/admin/user/flying',
+			icon: 'icon-globe',
+			children: [
+				{
+					name: 'Locations',
+					url: '/admin/user/flying/locations',
+					icon: 'icon-location-pin'
+				},
+				{
+					name: 'Events',
+					url: '/admin/user/flying/locations',
+					icon: 'icon-calendar'
+				}
+			]
+		},
+		{
+			name: 'Friends',
+			url: '/admin/user/friends',
+			icon: 'icon-people'
+		}
+	]
+}
