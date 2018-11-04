3c19dab7ee5a43da521442509fae44513c756396
Brandon Tassone <brandontassone@gmail.com>
2018-11-04 00:37:38 -0400

    Removed old home page styles / html from original project

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,33 +1,8 @@
 <style>
-    #welcome {
-        width: 100vw;
-        height: 100vh;
-    }
 
-    #account-actions {
-        background-color: #ffffff;
-    }
-
-    #form-view-container {
-        background-color: #046380;
-    }
 </style>
 
 <template>
-    <div id="welcome" class="row m-0">
-        <div id="account-actions" class="col-md d-flex align-items-center justify-content-center">
-            <div class="d-flex flex-column w-25">
-                <h1>Laravel SPA Base</h1>
-                <div class="home-links">
-                    <router-link to="/login" class="btn btn-primary w-100 mb-3">Login</router-link>
-                    <router-link to="/register" class="btn btn-secondary w-100">Register</router-link>
-                </div>
-            </div>
-        </div>
-        <div id="form-view-container" class="col-md align-items-center justify-content-center d-none">
-            <router-view></router-view>
-        </div>
-    </div>
 </template>
 
 <script>
